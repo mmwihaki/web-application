bb6139c5ab11e9742504e112c735d6fdea653d62
Mary Mwihaki <marymwihaki16@gmail.com>
2020-07-20 15:24:47 +0300

    month of birth validation

--- js/namegenerator.js
+++ js/namegenerator.js
@@ -1,8 +1,10 @@
 function getName() {
-  let yearOfBirth = document.getElementByID(yearOfBirth).value);
-
+  let yearOfBirth = document.getElementByID("yearOfBirth").value);
+  let monthOfBirth = Number (document.getElementByID("month-input").value);
+  let dayOfBirth = Number (document.getElementByID("day-input").value);
   let genders = documents.getElementsByName("gender");
 
+  //gender function
   function getGender () {
     for (let gender of genders){
       if (gender.checked){
@@ -14,14 +16,19 @@ function getName() {
   let myGenderValue = getGender ();
   console.log(myGenderValue);
 
+//validation
   function Validator(){
     if (monthOfBirth <1 || monthOfBirth > 12) {
       return false;
     } else {
       return true;
     }
-
-    }
   }
 
+function dayValidator () {
+  if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+    if (dayOfBirth > 28 || dayOfBirth < 1) {
+      return false;
+    }
+  }
 }
