fcf2969f52364f3c234d9be7dc9e4abfecfeb9bf
Mary Mwihaki <marymwihaki16@gmail.com>
2020-07-23 21:20:05 +0300

    century calculator

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 var yearOfBirth = 1996;
 var monthOfBirth = document.getElementById("monthOfBirth");
-var dayOfBirth = 31;
+var dayOfBirth = 14;
 
 
-//vaidating date
-if (dayOfBirth <= 0 || 31){
+//vaidating dateOfBirth
+if (dayOfBirth >= 1 && dayOfBirth <= 31){
   console.log("Awesome!");
-} else {
+}else {
   console.log("please enter a valid date");
 }
+
+//validating monthOfBirth
+if (monthOfBirth >= 1 && monthOfBirth <= 12){
+  console.log("Awesome!");
+}else {
+  console.log("please enter a valid date");
+}
+
+//century from yearOfBirth
+
+var century = yearOfBirth % 100 === 0 ? year/100 : Math.floor(yearOfBirth/100) + 1;
+console.log(century);
