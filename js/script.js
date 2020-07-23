aad0f0c11100608b19fdadd2ec993375f3ad3499
Mary Mwihaki <marymwihaki16@gmail.com>
2020-07-23 20:41:00 +0300

    date validator

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var yearOfBirth = 1996;
+var monthOfBirth = document.getElementById("monthOfBirth");
+var dayOfBirth = 31;
+
+
+//vaidating date
+if (dayOfBirth <= 0 || 31){
+  console.log("Awesome!");
+} else {
+  console.log("please enter a valid date");
+}
